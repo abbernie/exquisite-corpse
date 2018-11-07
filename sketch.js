d6acad5038750df6ed603b127770592c48478d15
Andrew Bernstein <andrewbbernstein@gmail.com>
2018-11-07 11:30:25 -0500

    hello

--- sketch.js
+++ sketch.js
@@ -6,11 +6,7 @@ function setup() {
 }
 
 function draw() {
-  background(255);
-  noStroke();
 
-  var opacity = map(mouseX, 0, width, 0, 255);
-
-  fill(255,160,10, opacity);
-  ellipse(width/2, height/2, width);
+  print("hello!");
+  
 }
