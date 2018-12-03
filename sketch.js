a004df1b9621fdfc5a49a139734a33a781f8d3d5
Molly <molly.margulies@gmail.com>
2018-12-02 21:53:22 -0500

    Added background image
    Added a series of layered rectangles as a backdrop for the exquisite corpse.

--- sketch.js
+++ sketch.js
@@ -6,7 +6,16 @@ function setup() {
 }
 
 function draw() {
-
+  background(0)
   print("Hello World!");
 
+  noStroke()
+  fill(140, 150, 60);
+  rect(25, 25, 350, 350);
+  fill(100, 110, 80);
+  rect(50, 50, 300, 300);
+  fill(0);
+  rect(75, 75, 250, 250);
+  fill(140,150, 60);
+  rect(100, 100, 200, 200);
 }
